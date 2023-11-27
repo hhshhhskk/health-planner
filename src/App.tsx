2bd2be10b273f3ce46e6e1017e9dc2a7200eafe8
hhshhhskk <hhshhhskk@naver.com>
2023-11-27 17:27:27 +0900

    register 활용연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Planner from "./components/Planner";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Planner />
+    </div>
+  );
 }
 
 export default App;
